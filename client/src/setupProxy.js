ac14c71acc9e6056a266fe476fda5739e8c2c82a
Yusen <65815304+kulakaka@users.noreply.github.com>
2024-11-06 02:31:06 -0600

    update proxy config
    Signed-off-by: Yusen <65815304+kulakaka@users.noreply.github.com>

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     app.use(
       '/api',
       createProxyMiddleware({
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5000'||'https://formalyze-client.vercel.app',
         changeOrigin: true,
         timeout: 30000,
         proxyTimeout: 31000,
