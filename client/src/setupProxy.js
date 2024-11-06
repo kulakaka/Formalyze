5419f406ec635f49282413c947c5da0a07add567
Yusen <65815304+kulakaka@users.noreply.github.com>
2024-11-06 00:06:50 -0600

    remove starting script
    Signed-off-by: Yusen <65815304+kulakaka@users.noreply.github.com>

---
+++ client/src/setupProxy.js
@@ -0,0 +1,17 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+      timeout: 30000,
+      proxyTimeout: 31000,
+      onError: (err, req, res) => {
+        console.error('Proxy Error:', err);
+        res.status(500).send('Proxy Error');
+      }
+    })
+  );
+}; 
--- server/index.js
+++ server/index.js
@@ -5,7 +5,16 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const groqRoutes = require('./routes/groq');
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use((req, res, next) => {
+    res.setTimeout(30000, () => {
+        res.status(504).send('Server Timeout');
+    });
+    next();
+});
+
 app.use('/api', groqRoutes);
 
 app.get('/', (req, res) => {
