cf21538bf7346b3437ed3d0f2d6d614de88f9bd7
Yusen <65815304+kulakaka@users.noreply.github.com>
2024-11-06 21:10:40 -0600

    update all
    Signed-off-by: Yusen <65815304+kulakaka@users.noreply.github.com>

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
     timeout: 30000,
     headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'text/plain;charset=utf-8',
     },
     withCredentials: true
 });
